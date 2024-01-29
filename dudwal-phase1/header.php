896a58e9ec296c6fe63849aed92d95ab1690fc50
kamryndudwal <95425654+kamryndudwal@users.noreply.github.com>
2024-01-29 13:15:05 +0000

    Uploading assignment as is

---
+++ dudwal-phase1/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav>
+            <a href="phase1.php"><b>Kamryn Dudwal</b></a>
+            <a href="experience.php">Experience</a>
+            <a href="skills.php">Skills</a>
+           <a href="portfolio.php">Portfolio</a>
+            <a href="login.php">Login</a>
+    </nav>
+    <br><br><br/>
---
+++ dudwal-phase1/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <nav>
+            <a href="phase1.html"><b>Kamryn Dudwal</b></a>
+            <a href="experience.html">Experience</a>
+            <a href="skills.html">Skills</a>
+           <a href="portfolio.html">Portfolio</a>
+            <a href="login.html">Login</a>
+    </nav>
+    <br><br><br/>
+
+    <aside>
+        <img src="remy.jpg" alt="image is not appearing" />  
+    </aside>
+
+    <article>
+        <div id=aboutme>
+            <h1>About Me</h1>
+            <p>Hi, I am a current student studying Creative Computing at Queen Mary's University of London.</p>
+        </div>
+        <br><br>
+        <div id=edu>
+        <h1>Education</h1>
+            <p><b>2021-2024</b> Queen Marys University </p>
+            <p><b>2017-2021</b> High School Diplomas- Las Lomas High School</p>
+        </div>
+    </article>
+    <br><br><br><br>
+    
+    <footer>
+        <ul>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </footer>
+</body>
+</html>
